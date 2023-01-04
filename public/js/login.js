8ea18c2366d6d6bc5f62d3896d077299afbc90c6
Jeevan Johal <jkjohal@ucdavis.edu>
2023-01-03 19:53:48 -0800

    changed ids

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#login-userName').value.trim();
+    const password = document.querySelector('#login-password').value.trim();
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
@@ -22,14 +22,13 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#signup-userName').value.trim();
+    const password = document.querySelector('#signup-password').value.trim();
   
-    if (username && email && password) {
+    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
