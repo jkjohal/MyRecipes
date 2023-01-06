b39e9c9ef1957fc8143a541e1c7d7033b73fe4d4
Dionne Angeles <jr8dionne@yahoo.com>
2023-01-05 20:20:41 -0800

    modified loginRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app =express();
 const PORT = 3001;
 
-const userRoute = require("./routes/User");
-const commentsRoute = require("./routes/Comments");
+const userRoute = require("./api/User");
+const commentsRoute = require("./api/Comments");
 app.use("/user", userRoute);
 app.use("/comments", commentsRoute);
 
